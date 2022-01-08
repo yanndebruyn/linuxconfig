d446a3746be5b622e944ee09e62fe5aba8cce966
yanndebruyn <public@yanndebruyn.com>
2022-01-08 20:20:36 +0100

    update

--- homedir/bin/scripts/dmenu_select_runscript.sh
+++ homedir/bin/scripts/dmenu_select_runscript.sh
@@ -4,5 +4,5 @@ TARGET=$(find /home/yann/bin/scripts/ \( -name "*sh" \) | cut -d/ -f6 |\
  	sort | dmenu -i -nb "#1b2235" -sb "#874f5c" -fn 'monospace:bold:pixelsize=20' -l 41 -p "Run script")
 
 if [ $TARGET != " " ]; then
-	xterm -e '/home/yann/bin/scripts/'$TARGET
+	alacritty -e '/home/yann/bin/scripts/'$TARGET
 fi
