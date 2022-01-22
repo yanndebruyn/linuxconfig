688ef1c0fa45d153caa4d23e9b244b63b4cd9969
yanndebruyn <public@yanndebruyn.com>
2022-01-22 12:29:36 +0100

    update

--- homedir/bin/scripts/dmenu_select_editscript.sh
+++ homedir/bin/scripts/dmenu_select_editscript.sh
@@ -4,5 +4,5 @@ TARGET=$(find /home/yann/bin/scripts/ \( -name "*sh" \) | cut -d/ -f6 | sort |\
  	dmenu -i -nb "#1b2235" -sb "#874f5c" -fn 'monospace:bold:pixelsize=20' -l 41 -p "Edit script")
 
 if [ $TARGET != " " ]; then
-	alacritty -e /bin/bash -c "stty -ixon; vim /home/yann/bin/scripts/$TARGET"
+	alacritty -e /bin/bash -c "stty -ixon; nvim /home/yann/bin/scripts/$TARGET"
 fi
